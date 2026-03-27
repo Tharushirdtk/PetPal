876a4f557bbe9ca0524b9aae346ce84ae6f932f7
Tharushirdtk <kavindirdtk@gmail.com>
2026-03-27 08:50:41 +0530

    feat: add React application core files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './i18n/LanguageContext';
+import { AuthProvider } from './context/AuthContext';
+import { ConsultationProvider } from './context/ConsultationContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PetDashboard from './pages/PetDashboard';
+import MedicalHistory from './pages/MedicalHistory';
+import ChatbotPage from './pages/ChatbotPage';
+import QuestionnairePage from './pages/QuestionnairePage';
+import ImageAnalysisPage from './pages/ImageAnalysisPage';
+import DiagnosisReportPage from './pages/DiagnosisReportPage';
+import AdminDashboard from './pages/AdminDashboard';
+
+function App() {
+  return (
+    <LanguageProvider>
+      <AuthProvider>
+        <ConsultationProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/dashboard" element={<ProtectedRoute><PetDashboard /></ProtectedRoute>} />
+              <Route path="/records" element={<ProtectedRoute><MedicalHistory /></ProtectedRoute>} />
+              <Route path="/chat" element={<ChatbotPage />} />
+              <Route path="/questionnaire" element={<QuestionnairePage />} />
+              <Route path="/image-analysis" element={<ImageAnalysisPage />} />
+              <Route path="/report" element={<DiagnosisReportPage />} />
+              <Route path="/admin" element={<ProtectedRoute adminOnly><AdminDashboard /></ProtectedRoute>} />
+            </Routes>
+          </BrowserRouter>
+        </ConsultationProvider>
+      </AuthProvider>
+    </LanguageProvider>
+  );
+}
+
+export default App;
